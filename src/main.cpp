0576efc91c17d7bc952111e27ed2b418c1c6d289
SayidolimNUU <127468262+SayidolimNUU@users.noreply.github.com>
2023-10-17 19:49:19 +0500

    first commit
    can't commit through IDE, changing file directly

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,29 @@
 /*
- * Author:
- * Date:
- * Name:
+ * Author:  Sayidolimxon Usmanxonov
+ * Date:    17.10.2023
+ * Name:    Sayidolimxon Usmanxonov
  */
 
 #include <iostream>
 #include "task_1.h"
 
 int main() {
-    std::cout << "Task 1" << std::endl;
-    // call for task 1
-    std::cout << "Task 2" << std::endl;
-    // call for task 2
-    std::cout << "Task 3" << std::endl;
-    // call for task 3
-    std::cout << "Task 4" << std::endl;
-    // call for task 4
-    std::cout << "Task 5" << std::endl;
-    // call for task 5
+    //Testing class Family:
+    Family myFamily;
+    myFamily.extendedFamily = true;
+    myFamily.numberOfMembers = 7;
+    myFamily.numberOfChildren = 3;
+    myFamily.income = 700;
+    std::cout << "This family is " << (myFamily.extendedFamily ? "" : "not ") << "extended,\n"
+    << "has " << myFamily.numberOfMembers << " members,\n"
+    << "has " << myFamily.numberOfChildren << " children and " << myFamily.numberOfAdults() << " adults.\n"
+    << "It makes " << myFamily.income << " dollars monthly.\n"
+    << "The parents are " << (myFamily.getIfDivorced() ? "" : "not ") << "divorced" << std::endl;
+
+    //Testing class Children:
+    Children myChildren;
+    myChildren.numberOfMembers = 3;
+    std::cout << "\nThere are " << myChildren.numberOfMembers << " children and "
+    << myChildren.numberOfAdults() << " adults in this object" << std::endl;
     return 0;
 }
